fdcbaac6a239ad4dffbceef912271bbe9aa7d143
bluberdiblu <r.pommer@arcor.de>
2018-03-10 00:27:58 +0100

    edited Server

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -6,26 +6,27 @@ import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Server {		
+public class Server {	
+	ExecutorService executor;
+	ServerSocket server;
 	
-	public void start() {	
-		
+	public void start() {
 		System.out.println("Starting Server...");
-		ExecutorService executor = Executors.newFixedThreadPool(100);	
-		ServerSocket server;
+		executor = Executors.newFixedThreadPool(100);		
+		
 		try {
-			server = new ServerSocket();
+			server = new ServerSocket(5555);
 			
-			while(true) {
+			while(!server.isClosed()) {
 				Socket client = server.accept();
 				executor.execute(new Clienthandler(client));
-				
 			}
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}	
+	}
+	
 	
 	public static void main(String[] args) {
 		new Server().start();
